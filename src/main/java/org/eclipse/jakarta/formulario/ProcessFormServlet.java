1002ddb03e3608df7ca698bc64a40483232403d8
Wemerson-ferr <wemerson-b@hotmail.com>
2024-05-03 18:10:57 -0300

    final

--- src/main/java/org/eclipse/jakarta/formulario/ProcessFormServlet.java
+++ src/main/java/org/eclipse/jakarta/formulario/ProcessFormServlet.java
@@ -1,65 +1,57 @@
 package org.eclipse.jakarta.formulario;
 
-import jakarta.servlet.*;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.POST;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
-
-import java.io.*;
 
 @WebServlet("/cadastrar")
 public class ProcessFormServlet extends HttpServlet {
 
     @POST
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String nome = request.getParameter("nome");
-        String sobrenome = request.getParameter("sobrenome");
-        String idade = request.getParameter("idade");
-        String telefone = request.getParameter("telefone");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            // Definindo o cabeçalho para forçar o download
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "attachment; filename=relatorio.pdf");
 
-        // Crie um objeto Pessoa
-        Pessoa pessoa = new Pessoa(nome, sobrenome, idade, telefone);
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.getInstance(document, response.getOutputStream());
+            document.open();
 
-        // Crie um documento PDF
-        PDDocument document = new PDDocument();
-        PDPage page = new PDPage();
-        document.addPage(page);
+            // Recuperando os dados do request
+            String nome = request.getParameter("nome");
+            String sobrenome = request.getParameter("sobrenome");
+            String idade = request.getParameter("idade");
+            String telefone = request.getParameter("telefone");
 
-        // Adicione o conteúdo JSON à página
-        PDPageContentStream contentStream = new PDPageContentStream(document, page);
-        contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD), 15);
+            // Adicionando os dados ao documento
+            Font font = FontFactory.getFont(FontFactory.HELVETICA, "UTF-8");
+            document.addTitle("Relatório de cadastro");
+            document.add(new Paragraph("Nome: " + nome, font));
+            document.add(new Paragraph("Sobrenome: " + sobrenome, font));
+            document.add(new Paragraph("Idade: " + idade, font));
+            document.add(new Paragraph("Telefone: " + telefone, font));
 
-        // Codifique o texto em UTF-8
-        String jsonText = pessoa.toJson();
-        byte[] utf8Bytes = jsonText.getBytes("UTF-8");
+            document.close();
 
-        // Adicione o texto com caracteres especiais
-        contentStream.beginText();
-        contentStream.newLineAtOffset(50, 700); // Posição X e Y
-        contentStream.showText(new String(utf8Bytes, "UTF-8"));
-        contentStream.endText();
-        contentStream.close();
-
-        // Salve o documento como um arquivo PDF
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        document.save(byteArrayOutputStream);
-        document.close();
-
-        // Defina o cabeçalho de resposta para download
-        response.setContentType("application/pdf");
-        response.setHeader("Content-Disposition", "attachment; filename=pessoa.pdf");
-
-        // Escreva o conteúdo do PDF na resposta
-        OutputStream outputStream = response.getOutputStream();
-        byteArrayOutputStream.writeTo(outputStream);
-        outputStream.flush();
-        outputStream.close();
+            Pessoa user = new Pessoa(nome, sobrenome, idade, telefone);
+            
+            System.out.println(user.toJson());
+        } catch (DocumentException e) {
+            throw new IOException(e.getMessage());
+        }
     }
 }
